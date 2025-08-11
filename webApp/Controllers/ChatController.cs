c9db656c8d13288ee4760e8cbc10e480b88f229e
Alejandro Paniagua <xxelmerxx15@gmail.com>
2025-08-11 16:59:25 -0600

    Chat y Grupos

---
+++ webApp/Controllers/ChatController.cs
@@ -0,0 +1,47 @@
+﻿using SmartSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartSys.Controllers
+{
+    public class ChatController : Controller
+    {
+        SmartSysDbContext db = new SmartSysDbContext();
+        [HttpGet]
+        public ActionResult Index(string busqueda = "")
+        {
+            db.Database.CommandTimeout = 180;
+            // Obtener usuarios
+            var usuarios = db.Usuario.AsNoTracking().AsQueryable();
+
+            // Filtrar si se ha hecho una búsqueda
+            if (!string.IsNullOrWhiteSpace(busqueda))
+            {
+                busqueda = busqueda.ToLower(); // Normalizar a minúsculas
+
+                usuarios = usuarios.Where(u =>
+                    u.Nombre.ToLower().Contains(busqueda) ||
+                    u.Apellido.ToLower().Contains(busqueda));
+            }
+
+            ViewBag.Busqueda = busqueda;
+
+            return View(usuarios.ToList());
+        }
+        public ActionResult Grupos()
+        {
+            return View();
+        }
+        public ActionResult ChatMensajes()
+        {
+            return View();
+        }
+        public ActionResult ChatGrupo()
+        {
+            return View();
+        }
+    }
+}
